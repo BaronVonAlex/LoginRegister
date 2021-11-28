72291635a9a7a6a303f6e93962d998537372940f
Baron <aleko.kotliarovi@gmail.com>
2021-11-28 23:09:16 +0400

    yes

---
+++ app/src/main/java/com/example/loginregister/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.example.loginregister
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.content.Intent
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var truemail = "alex@gmail.com"
+    private var truepass = "123123"
+
+    private lateinit var email1: EditText
+    private lateinit var password1: EditText
+    private lateinit var login1: Button
+    private lateinit var signup: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        email1 = findViewById(R.id.email1)
+        password1 = findViewById(R.id.password1)
+        login1 = findViewById(R.id.login1)
+        signup = findViewById(R.id.signup)
+
+        login1.setOnClickListener{
+            if (email1.text.toString() == truemail && password1.text.toString() == truepass){
+                Toast.makeText(this,"Correct info",Toast.LENGTH_SHORT).show()
+            }else Toast.makeText(this,"Wrong info",Toast.LENGTH_SHORT).show()
+        }
+
+        signup.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/example/loginregister/MainActivity2.kt
@@ -0,0 +1,4 @@
+package com.example.loginregister
+
+class MainActivity2 {
+}
